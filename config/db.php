0aafc4b818b4772b1c70b9873f74343244452a6e
Khizar Aamir <champkhiz04@gmail.com>
2025-02-19 16:56:53 -0800

    Initial project structure setup

---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+// Database configuration placeholder
+$servername = "localhost";
+$username = "root"; // Change for production
+$password = ""; // Change for production
+$dbname = "handmade_store";
+?>
