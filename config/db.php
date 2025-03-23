1e4b7c1c7c2bffc5f1f049852461079ef55ab082
Abdur <abdur026@gmail.com>
2025-03-23 04:14:44 -0700

    Fix db.php to always use localhost for database connections

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 // Basic config
-$servername = "localhost";
+$servername = "localhost"; // Always use localhost for MySQL connections
 $username = "qrehman";
 $password = "qrehman";
 $dbname = "qrehman";
@@ -10,6 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
+    error_log("Database connection failed: " . $conn->connect_error);
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
