b19df06bbf7d2645e17d62c16681d37ac3d7416e
Abdur <abdur026@gmail.com>
2025-04-09 15:39:27 -0700

    Update database configuration to handle both local and server environments

--- config/db.php
+++ config/db.php
@@ -1,9 +1,20 @@
 <?php
+// Determine if we're running on the UBC server or locally
+$is_ubc_server = (strpos($_SERVER['HTTP_HOST'], 'cosc360.ok.ubc.ca') !== false);
+
 // Database configuration
 $servername = "localhost";
-$username = "qrehman"; // UBC server username
-$password = "qrehman"; // UBC server password
-$dbname = "qrehman"; // UBC server database name
+if ($is_ubc_server) {
+    // UBC server credentials
+    $username = "qrehman";
+    $password = "qrehman";
+    $dbname = "qrehman";
+} else {
+    // Local development credentials
+    $username = "root";  // Your local MySQL username
+    $password = "";      // Your local MySQL password
+    $dbname = "artisenalley";  // Your local database name
+}
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
